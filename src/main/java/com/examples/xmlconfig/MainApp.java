ceb42184ff4172c559d775b2c0752948bfa2c223
Krishna Bahadur Bhat <krishna.bhat78@gmail.com>
2017-05-16 22:28:09 -0400

    persist/save Employee objects to database

--- src/main/java/com/examples/xmlconfig/MainApp.java
+++ src/main/java/com/examples/xmlconfig/MainApp.java
@@ -1,7 +1,12 @@
 package com.examples.xmlconfig;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.io.Serializable;
 
 /**
  * Created by krishna1bhat on 5/16/17.
@@ -10,11 +15,49 @@ public class MainApp {
     private static SessionFactory sessionFactory;
 
     public static void main(String[] args){
+        //this step will read hibernate.cfg.xml and prepare for use
         try{
             sessionFactory = new Configuration().configure().buildSessionFactory();
         }catch (Throwable e){
             System.out.println("Can not create sessionFactory object: " + e);
             throw new ExceptionInInitializerError(e);
         }
+
+        //hibernate placeholders
+        Session session = null;
+        Transaction tx = null;
+
+        //open session
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+
+            //create new Employee and set values
+            Employee emp1 = new Employee();
+            emp1.setFirstName("Krishna");
+            emp1.setLastName("Bhat");
+            emp1.setSalary(99999);
+
+            //save emp
+            //persist() - no return type
+            session.persist(emp1);
+
+            //save() -- returns generated id
+            Employee emp2 = new Employee("John", "Oran", 88888);
+            Serializable save = session.save(emp2);
+            System.out.println("Generated identifier: " + save);
+
+            tx.commit();
+
+        }catch (HibernateException e){
+            if(tx != null)
+                tx.rollback();
+            e.printStackTrace();
+        }finally {
+            if(session != null)
+                session.close();
+        }
+
+        sessionFactory.close();
     }
 }
